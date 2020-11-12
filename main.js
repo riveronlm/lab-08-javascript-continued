a78c640ccad057386a7b709341854896567e84c0
luis <luisriveron1213@gmail.com>
2020-11-11 19:13:15 -0500

    completed variables

---
+++ main.js
@@ -0,0 +1,10 @@
+var order = 66;
+var droid = 'R2D2';
+var isthisthedroidyourlookingfor = false;
+var clonewars = null;
+var Jediknights;
+
+console.log(order);
+console.log(droid);
+console.log(isthisthedroidyourlookingfor);
+console.log(Jediknights);
